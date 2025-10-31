1c61b2575cfdb0530217ce172b46bbcaecfb14d7
Archana <archanadby05@gmail.com>
2025-10-31 22:43:52 +0530

    Add Cassandra schema and data loader
    - Creates keyspace 'stocks' and table 'stock_summary'
    - Adds cassandra/load_data.py to insert processed daily summaries
    - Prepares database for analytics queries

---
+++ cassandra/load_data.py
@@ -0,0 +1,26 @@
+import csv
+from cassandra.cluster import Cluster
+from datetime import datetime
+
+cluster = Cluster(['127.0.0.1'])
+session = cluster.connect('stockks')
+
+insert_stmt = session.prepare("""
+    INSERT INTO stock_summary (symbol, date, year, month, close, daily_return)
+    VALUES (?, ?, ?, ?, ?, ?)
+""")
+
+with open("../output/daily_summary/part-00000.csv", newline='') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        session.execute(insert_stmt, (
+            row['Symbol'],
+            datetime.strptime(row['DATE'], "%Y-%m-%d").date(),
+            int(row['year']),
+            int(row['month']),
+            float(row['Close']),
+            float(row['daily_return'])
+        ))
+
+cluster.shutdown()
+print("Data loaded into Cassandra successfully.")
